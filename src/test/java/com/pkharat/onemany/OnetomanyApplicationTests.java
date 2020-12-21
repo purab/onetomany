02515c975e65ec6d8725bb9b4fbdeaf565e0bf47
DELL <purabdk@gmail.com>
2020-12-21 23:46:50 +0530

    add project files to one to many mapping

---
+++ src/test/java/com/pkharat/onemany/OnetomanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pkharat.onemany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetomanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
